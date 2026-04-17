75bb9ad8d6e0003ea3da93853d94f153e31bf346
olium <jeremie.rouxel@outlook.fr>
2026-04-17 16:30:23 +0200

    ajout du .h

---
+++ simple_shell.h
@@ -0,0 +1,12 @@
+#ifndef SIMPLE_SHELL_H
+#define SIMPLE_SHELL_H
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+
+
+#endif
