c8b24f57914ead2efca05daa1408712ffd7f62f8
olium <jeremie.rouxel@outlook.fr>
2026-04-19 11:33:30 +0200

    mise a jour du simple_shell.h

--- simple_shell.h
+++ simple_shell.h
@@ -1,5 +1,12 @@
+/*
+*simple_shell.h,
+*This header regroups all the required prototypes and librairies needed,
+*for the simple_shell project
+*/
+
 #ifndef SIMPLE_SHELL_H
 #define SIMPLE_SHELL_H
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -7,6 +14,7 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 
+extern char **environ;
+int copy_idandexe(char *command);
 
-
-#endif
+#endif /*SIMPLE_SHELL_H*/
