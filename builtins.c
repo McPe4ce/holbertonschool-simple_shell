b0e8cf178173d79b242e8a4d4d4be3ebbe17620a
Mcpe4ce <phil.ghanem@gmail.com>
2026-04-20 13:05:23 +0200

    Refactor handle_builtin function documentation and formatting

--- builtins.c
+++ builtins.c
@@ -1,13 +1,18 @@
 #include "simple_shell.h"
 
-
+/**
+ * handle_builtin - The exit builtin is handled here and ends process
+ * @args: arguments parsed
+ * @line: the command to execute
+ * Return: 0 (Success)
+ */
 int handle_builtin(char **args, char *line)
 {
-    if (_strcmp(args[0], "exit") == 0)
-    {
-        free_exec(args, NULL);
-        free(line);
-        exit(0);
-    }
-    return (0);
+	if (_strcmp(args[0], "exit") == 0)
+	{
+		free_exec(args, NULL);
+		free(line);
+		exit(0);
+	}
+	return (0);
 }
